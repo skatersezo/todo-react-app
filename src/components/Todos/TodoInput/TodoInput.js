5b06a6ffae744cae0a37d923579cd200d89c3289
Ivan <ivansezoskate@gmail.com>
2021-05-18 03:08:57 +0900

    Adds dynamic classes

--- src/components/Todos/TodoInput/TodoInput.js
+++ src/components/Todos/TodoInput/TodoInput.js
@@ -6,24 +6,33 @@ import './TodoInput.css';
 function TodoInput(props) {
 
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const todoInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
+      setIsValid(false);
       return;
     }
     props.onAddTodo(enteredValue);
+    setIsValid(true);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>To-do</label>
-        <input type="text" onChange={todoInputChangeHandler} />
+        <input 
+          type="text" 
+          onChange={todoInputChangeHandler} 
+        />
       </div>
       <Button type="submit">Add Todo</Button>
     </form>
