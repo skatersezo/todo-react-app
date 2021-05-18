b99256bbb46d74bbbf56a4f752f8fd135bc5ea08
Ivan <ivansezoskate@gmail.com>
2021-05-18 22:45:38 +0900

    Uses React CSS modules in the button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,10 @@
 
-import './Button.css';
+import styles from './Button.module.css';
 
 
 function Button(props) {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
