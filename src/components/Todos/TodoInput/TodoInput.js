f046481136d6fd9e59b8f59ee6efe8b523df8e4c
Ivan <ivansezoskate@gmail.com>
2021-05-18 18:44:21 +0900

    Applies styled components to TodoInput form control

--- src/components/Todos/TodoInput/TodoInput.js
+++ src/components/Todos/TodoInput/TodoInput.js
@@ -1,7 +1,43 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './TodoInput.css';
+
+const FormControl = styled.div`
+
+  margin: 0.5rem 0;
+
+
+  & label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+  & input {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+  & input:focus {
+  outline: none;
+  background: #fad0ec;
+  border-color: #8b005d;
+}
+
+  &.invalid input {
+  border-color: red;
+  background: #ffd7d7;
+}
+
+  &.invalid label {
+  color: red;
+}
+`;
 
 function TodoInput(props) {
 
@@ -27,13 +63,13 @@ function TodoInput(props) {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl className={!isValid && 'invalid'}>
         <label>To-do</label>
         <input 
           type="text" 
           onChange={todoInputChangeHandler} 
         />
-      </div>
+      </FormControl>
       <Button type="submit">Add Todo</Button>
     </form>
   );
