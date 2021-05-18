63c38a0e29c0eaacfb6c7e4940a58502631879f1
Ivan <ivansezoskate@gmail.com>
2021-05-18 20:18:50 +0900

    Reverts the usage of Styled components library

--- src/components/Todos/TodoInput/TodoInput.js
+++ src/components/Todos/TodoInput/TodoInput.js
@@ -1,36 +1,6 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-
-const FormControl = styled.div`
-
-  margin: 0.5rem 0;
-
-
-  & label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color: ${props => (props.invalid ? 'red' : 'black')};
-}
-
-  & input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-  background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
-
-  & input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
-`;
 
 function TodoInput(props) {
 
@@ -38,10 +8,10 @@ function TodoInput(props) {
   const [isValid, setIsValid] = useState(true);
 
   const todoInputChangeHandler = event => {
+    setEnteredValue(event.target.value);
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
-    setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
@@ -56,13 +26,10 @@ function TodoInput(props) {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>To-do</label>
-        <input 
-          type="text" 
-          onChange={todoInputChangeHandler} 
-        />
-      </FormControl>
+        <input type="text" onChange={todoInputChangeHandler} />
+      </div>
       <Button type="submit">Add Todo</Button>
     </form>
   );
